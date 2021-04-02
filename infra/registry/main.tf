9be9a0a3bf2985640fec38b1b5784d1d8607c25d
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-04-02 15:56:28 +0200

    add registry bucket

--- infra/registry/main.tf
+++ infra/registry/main.tf
@@ -2,13 +2,18 @@ variable "environment_name" {
   type = string
 }
 
+variable "project_prefix" {
+  type = string
+}
+
 locals {
   environment_suffix = title(var.environment_name)
-  name = "RegistryTable${local.environment_suffix}"
-  organization_status_index_name = "OrganizationStatus${local.environment_suffix}"
+  bucket = "${var.project_prefix}registry-${local.environment_suffix}"
+  lookup_table = "RegistryLookUp${local.environment_suffix}"
+  organization_status_index = "OrganizationStatus${local.environment_suffix}"
 }
-resource "aws_dynamodb_table" "basic-dynamodb-table" {
-  name           = local.name
+resource "aws_dynamodb_table" "lookup_table" {
+  name           = local.lookup_table
   hash_key       = "identityKey"
   range_key      = "identityType"
   read_capacity  = 1
@@ -35,7 +40,7 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
   }
 
   global_secondary_index {
-    name               = local.organization_status_index_name
+    name               = local.organization_status_index
     hash_key           = "approvalStatus"
     range_key          = "organizationId"
     write_capacity     = 1
@@ -49,14 +54,26 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
   }
 }
 
+resource "aws_s3_bucket" "bucket" {
+  bucket = local.bucket
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+  tags = {
+    Environment = var.environment_name
+  }
+}
+
 resource "aws_ssm_parameter" "registry_name" {
-  name        = "/${var.environment_name}/registry/name"
+  name        = "/${var.environment_name}/registry/lookup_table"
   type        = "SecureString"
-  value       = local.name
+  value       = local.lookup_table
 }
 
 resource "aws_ssm_parameter" "organization_status_index_name" {
   name        = "/${var.environment_name}/registry/indices/organizationStatus"
   type        = "SecureString"
-  value       = local.organization_status_index_name
+  value       = local.organization_status_index
 }
