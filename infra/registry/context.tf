5055a100df4bb199555a49d18355e1d64c3a9615
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-04-08 16:41:03 +0200

    update parameters

--- infra/registry/context.tf
+++ infra/registry/context.tf
@@ -13,28 +13,29 @@ locals {
   event_bus = "${var.project_prefix}-${local.name}-event-bus-${var.environment_name}"
   lookup_table = "${var.project_prefix}-${local.name}-lookup-table-${var.environment_name}"
   organization_status_index ="${var.project_prefix}-organization-status-index-${var.environment_name}"
+  parameter_prefix="/${var.project_prefix}/${var.environment_name}/${local.name}"
 }
 
 resource "aws_ssm_parameter" "registry_event_bus" {
-  name        = "/${var.environment_name}/${local.name}/event_bus"
+  name        = "${local.parameter_prefix}/event_bus"
   type        = "SecureString"
   value       = local.event_bus
 }
 
 resource "aws_ssm_parameter" "registry_lookup_table_name" {
-  name        = "/${var.environment_name}/${local.name}/lookup_table"
+  name        = "${local.parameter_prefix}/lookup_table"
   type        = "SecureString"
   value       = local.lookup_table
 }
 
 resource "aws_ssm_parameter" "registry_bucket_name" {
-  name        = "/${var.environment_name}/${local.name}/bucket"
+  name        = "${local.parameter_prefix}/bucket"
   type        = "SecureString"
   value       = local.bucket
 }
 
 resource "aws_ssm_parameter" "organization_status_index_name" {
-  name        = "/${var.environment_name}/${local.name}/indices/organization_status"
+  name        = "${local.parameter_prefix}/indices/organization_status"
   type        = "SecureString"
   value       = local.organization_status_index
 }
