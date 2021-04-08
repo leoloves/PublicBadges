fc029d7dc0e6383e300e43a69712ecd03e754684
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-04-08 16:52:32 +0200

    FIX: update parameters

--- infra/api/context.tf
+++ infra/api/context.tf
@@ -18,7 +18,7 @@ locals {
 
 
 resource "aws_ssm_parameter" "role" {
-  name        = "/${local.parameter_prefix}/role"
+  name        = "${local.parameter_prefix}/role"
   type        = "SecureString"
   value       = aws_iam_role.api_role.arn
 }
