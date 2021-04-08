33ba0b3720aa47d9db44d08eab0b27cb7c1bf902
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-04-08 16:49:59 +0200

    FIX: update parameters

--- infra/badges/context.tf
+++ infra/badges/context.tf
@@ -14,13 +14,13 @@ locals {
 }
 
 resource "aws_ssm_parameter" "event_bus" {
-  name        = "/${local.parameter_prefix}/event_bus"
+  name        = "${local.parameter_prefix}/event_bus"
   type        = "SecureString"
   value       = local.event_bus
 }
 
 resource "aws_ssm_parameter" "bucket_name" {
-  name        = "/${local.parameter_prefix}/bucket"
+  name        = "${local.parameter_prefix}/bucket"
   type        = "SecureString"
   value       = local.bucket
 }
