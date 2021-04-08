e1de0caf29f209b764971a6b80e5e9a7ab61e9ce
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-04-08 16:45:34 +0200

    update parameters

--- infra/api/context.tf
+++ infra/api/context.tf
@@ -13,11 +13,12 @@ locals {
   name = "api"
   environment_suffix = title(var.environment_name)
   role = "${var.project_prefix}-${local.name}-role-${var.environment_name}"
+  parameter_prefix="/${var.project_prefix}/${var.environment_name}/${local.name}"
 }
 
 
 resource "aws_ssm_parameter" "role" {
-  name        = "/${var.environment_name}/${local.name}/role"
+  name        = "/${local.parameter_prefix}/role"
   type        = "SecureString"
   value       = aws_iam_role.api_role.arn
 }
--- infra/badges/context.tf
+++ infra/badges/context.tf
@@ -10,6 +10,7 @@ locals {
   name = "badges"
   bucket = "${var.project_prefix}-${local.name}-bucket-${var.environment_name}"
   event_bus = "${var.project_prefix}-${local.name}-event-bus-${var.environment_name}"
+  parameter_prefix="/${var.project_prefix}/${var.environment_name}/${local.name}"
 }
 
 resource "aws_ssm_parameter" "event_bus" {
@@ -19,7 +20,7 @@ resource "aws_ssm_parameter" "event_bus" {
 }
 
 resource "aws_ssm_parameter" "bucket_name" {
-  name        = "/${var.environment_name}/${local.name}/bucket"
+  name        = "/${local.parameter_prefix}/bucket"
   type        = "SecureString"
   value       = local.bucket
 }
