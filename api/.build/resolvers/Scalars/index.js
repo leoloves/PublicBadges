c65aa60f504fac3787e3ae712f9125f746c467bf
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-04-15 13:42:07 +0200

    add build scripts

--- api/.build/resolvers/Scalars/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EmailAddress = exports.URL = exports.JSON = exports.GUID = void 0;
-var graphql_scalars_1 = require("graphql-scalars");
-Object.defineProperty(exports, "EmailAddress", { enumerable: true, get: function () { return graphql_scalars_1.EmailAddressResolver; } });
-Object.defineProperty(exports, "GUID", { enumerable: true, get: function () { return graphql_scalars_1.GUIDResolver; } });
-Object.defineProperty(exports, "URL", { enumerable: true, get: function () { return graphql_scalars_1.URLResolver; } });
-Object.defineProperty(exports, "JSON", { enumerable: true, get: function () { return graphql_scalars_1.JSONResolver; } });
-//# sourceMappingURL=index.js.map
