7a0aa078b74bd6c1ad6c39ddf1bea587b8cc3208
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-04-23 22:21:14 +0200

    FIX: select eventbus by arn instead of name

--- infra/shared/parameters.tf
+++ infra/shared/parameters.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "event_bus" {
   name        = "${local.parameter_prefix}/event_bus"
   type        = "SecureString"
-  value       = local.event_bus
+  value       = aws_cloudwatch_event_bus.event_bus.arn
 }
