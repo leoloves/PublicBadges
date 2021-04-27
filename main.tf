fa167bc313fad7b528425e9e0c78ea980ae9e3d0
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-04-27 17:37:46 +0200

    FIX: add secret key

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ variable "TFC_WORKSPACE_NAME" {
   default = ""
 }
 
-variable "BADGES_SECRET_KEY" {
+variable "badges_secret_key" {
   type = string
 }
 
@@ -69,7 +69,7 @@ module "public-spaces-registry" {
 module "public-spaces-badges" {
   source           = "./infra/badges"
   environment_name = local.environment_name
-  secret_key       = var.BADGES_SECRET_KEY
+  secret_key       = var.badges_secret_key
   project_prefix   = "public-badges"
   policies = [
     module.public-spaces-registry.read_registry_bucket_policy,
