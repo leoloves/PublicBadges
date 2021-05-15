c9092b2e4f98b761f9647b3a93b04fc8ed9fa9c5
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-05-15 22:32:34 +0200

    move sender email to organization

--- registryFlow/src/approveOrganization/index.ts
+++ registryFlow/src/approveOrganization/index.ts
@@ -17,10 +17,9 @@ const approveOrganization: PublicBadgesHandler<
     switch (detailType) {
         case EV.ORGANIZATION_APPROVAL_REQUESTED: {
             const approverEmail = process.env.APPROVER_EMAIL;
-            const recipientEmail = detail.contact.email;
             const params = {
                 Destination: {
-                    ToAddresses: [recipientEmail],
+                    ToAddresses: [approverEmail],
                 },
                 Message: {
                     Body: {
