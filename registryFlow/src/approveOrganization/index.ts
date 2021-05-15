7b2295c8fe99708a6ae14e9fafd2fac33b999a00
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-05-15 21:30:09 +0200

    send email

--- registryFlow/src/approveOrganization/index.ts
+++ registryFlow/src/approveOrganization/index.ts
@@ -1,22 +1,40 @@
 import {
-  PublicBadgesEventType as EV,
-  OrganizationApprovalRequestedEvent,
-  PublicBadgesHandler,
+    PublicBadgesEventType as EV,
+    OrganizationApprovalRequestedEvent,
+    PublicBadgesHandler,
 } from "@public-badges/types";
+
+import AWS from "aws-sdk";
+const ses = new AWS.SES();
 
 export type InputEvent = OrganizationApprovalRequestedEvent;
 export type OutputEvent = null;
 
 const approveOrganization: PublicBadgesHandler<
-  InputEvent,
-  OutputEvent
-> = async ({detailType, detail}) => {
-  switch (detailType) {
-    case EV.ORGANIZATION_APPROVAL_REQUESTED: {
-      console.log(detail);
-      return null;
+    InputEvent,
+    OutputEvent
+> = async ({ detailType, detail }) => {
+    switch (detailType) {
+        case EV.ORGANIZATION_APPROVAL_REQUESTED: {
+            const params = {
+                Destination: {
+                    ToAddresses: ["yeehaa123@mac.com"],
+                },
+                Message: {
+                    Body: {
+                        Text: { Data: JSON.stringify(detail, null, 2) },
+                    },
+
+                    Subject: { Data: "Test Email" },
+                },
+                Source: process.env.APPROVAL_EMAIL,
+            };
+
+            const response = await ses.sendEmail(params).promise();
+            console.log(response);
+            return null;
+        }
     }
-  }
 };
 
 export default approveOrganization;
