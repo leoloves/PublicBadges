0d1c903368386782c9baf905d1e9632ba5c6d603
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-05-17 19:59:00 +0200

    refactor

--- registryFlow/src/sendNotifications/index.ts
+++ registryFlow/src/sendNotifications/index.ts
@@ -1,12 +1,12 @@
 import AWS from "aws-sdk";
-import { capitalize } from "voca";
+import {capitalize} from "voca";
 import {
-    PublicBadgesEventType as EV,
-    OrganizationApprovalRequestedEvent,
-    OrganizationRegistrationRequestedEvent,
-    OrganizationApprovedEvent,
-    PendingOrganization,
-    PublicBadgesHandler,
+  PublicBadgesEventType as EV,
+  OrganizationApprovalRequestedEvent,
+  OrganizationRegistrationRequestedEvent,
+  OrganizationApprovedEvent,
+  PendingOrganization,
+  PublicBadgesHandler,
 } from "@public-badges/types";
 import arTemplate from "./approvalRequestedTemplate";
 import createMail from "./mailTemplate";
@@ -14,46 +14,52 @@ import createMail from "./mailTemplate";
 const ses = new AWS.SES();
 
 export type InputEvent =
-    | OrganizationRegistrationRequestedEvent
-    | OrganizationApprovalRequestedEvent
-    | OrganizationApprovedEvent;
+  | OrganizationRegistrationRequestedEvent
+  | OrganizationApprovalRequestedEvent
+  | OrganizationApprovedEvent;
 export type OutputEvent = null;
 
+const sendEmail = async ({recipients, sender, body, subject}) => {
+  const email = createMail({recipients, sender, body, subject});
+  await ses.sendEmail(email).promise();
+};
+
 const sendNotifications: PublicBadgesHandler<InputEvent, OutputEvent> = async ({
-    detailType,
-    detail,
+  detailType,
+  detail,
 }) => {
-    const approverEmail = process.env.APPROVER_EMAIL;
-    const sender = approverEmail;
-    switch (detailType) {
-        case EV.ORGANIZATION_APPROVAL_REQUESTED: {
-            const organization = detail as PendingOrganization;
-            const organizationName = capitalize(detail.name);
-            const approveEmail = createMail({
-                recipients: [approverEmail],
-                sender,
-                subject: `${organizationName} applied for the PublicSpaces Registry`,
-                body: arTemplate({ organization, approverEmail }),
-            });
-            const sendEmail = createMail({
-                recipients: [detail.contact.email, detail.admin.email],
-                sender,
-                subject: `Your Application for ${organizationName} for the PublicSpaces Registry is Under Consideration`,
-                body: "Test",
-            });
-            await ses.sendEmail(approveEmail).promise();
-            await ses.sendEmail(sendEmail).promise();
-            return null;
-        }
-        case EV.ORGANIZATION_APPROVED: {
-            const recipients = [detail.contact.email, detail.admin.email];
-            const body = "Test";
-            const subject = `You were accepted to the PublicSpaces Registry`;
-            const email = createMail({ recipients, sender, body, subject });
-            await ses.sendEmail(email).promise();
-            return null;
-        }
+  const approverEmail = process.env.APPROVER_EMAIL;
+  const sender = approverEmail;
+  const organizationName = capitalize(detail.name);
+  switch (detailType) {
+    case EV.ORGANIZATION_APPROVAL_REQUESTED: {
+      await sendEmail({
+        recipients: [approverEmail],
+        sender,
+        subject: `${organizationName} applied for the PublicSpaces Registry`,
+        body: arTemplate({
+          organization: detail as PendingOrganization,
+          approverEmail,
+        }),
+      });
+      await sendEmail({
+        recipients: [detail.contact.email, detail.admin.email],
+        sender,
+        subject: `Your Application for ${organizationName} for the PublicSpaces Registry is Under Consideration`,
+        body: "Test",
+      });
+      return null;
     }
+    case EV.ORGANIZATION_APPROVED: {
+      await sendEmail({
+        recipients: [detail.contact.email, detail.admin.email, approverEmail],
+        sender,
+        subject: `${organizationName} was accepted to the PublicSpaces Registry`,
+        body: "Test",
+      });
+      return null;
+    }
+  }
 };
 
 export default sendNotifications;
