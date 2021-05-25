46cfa317c092755b564ed14ce23edd332e1dd68a
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-05-25 19:51:07 +0200

    better template

--- infra/registry/main.tf
+++ infra/registry/main.tf
@@ -75,7 +75,7 @@ resource "aws_iam_role" "registry_role" {
 
 resource "aws_ses_template" "approval_requested_template" {
   name    = "${var.project_prefix}-${local.name}-approval-requested-template"
-  subject = "Greetings, {{name}}!"
+  subject: "{{displayName}} was accepted to the PublicSpaces Registry"
   html    = file("${path.module}/templates/approval_requested.txt")
   text    = file("${path.module}/templates/approval_requested.txt")
 }
