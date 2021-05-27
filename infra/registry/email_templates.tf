54e267b6c40f10a91c634734f1a660bb60d6d8e6
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-05-27 12:08:51 +0200

    FIX: email templates

--- infra/registry/email_templates.tf
+++ infra/registry/email_templates.tf
@@ -1,13 +1,13 @@
 resource "aws_ses_template" "organization_approval_requested_template" {
   name    = "${var.project_prefix}-${local.name}-organization_approval-requested-template"
-  subject = "{{displayName}} was accepted to the PublicSpaces Registry"
+  subject = "{{displayName}} applied to the PublicSpaces Registry"
   html    = file("${path.root}/emailTemplates/compiled/organization_approval_requested.html")
   text    = file("${path.root}/emailTemplates/compiled/organization_approval_requested.md")
 }
 
 resource "aws_ses_template" "pending_organization_registration_template" {
   name    = "${var.project_prefix}-${local.name}-pending-organization-template"
-  subject = "{{displayName}} was accepted to the PublicSpaces Registry"
+  subject = "Your Application for {{displayName}} for the PublicSpaces Registry is Under Consideration"
   html    = file("${path.root}/emailTemplates/compiled/pending_organization_registration.html")
   text    = file("${path.root}/emailTemplates/compiled/pending_organization_registration.md")
 }
